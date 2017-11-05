b9ede26d6963c300d3bb4072a8b2550746d5da60
Anmol <iamanmol.007@outlook.com>
2017-11-06 03:48:10 +0530

    passport jwt

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
-var authenticate = require('./authenticate')
+var authenticate = require('./authenticate');
+var config = require('./config');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,7 +19,7 @@ var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useMongoClient: true
 });
@@ -43,34 +44,11 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 //app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', index);
 app.use('/users', users);
-
-function auth(req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-    } else {
-        next();
-    }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- models/dishes.js
+++ models/dishes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-require ('mongoose-currency').loadType(mongoose);
+require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
 const commentSchema = new Schema({
@@ -18,44 +18,44 @@ const commentSchema = new Schema({
     type: String,
     required: true
   }
-},{
+}, {
   timestamps: true
 });
 
 const dishSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    image: {
-      type: String,
-      required: true
-    },
-    category: {
-      type: String,
-      required: true
-    },
-    label: {
-      type: String,
-      default: ''
-    },
-    price: {
-      type: Currency,
-      required: true,
-      min: 0
-    },
-    featured: {
-      type: Boolean,
-      default: false
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    comments: [commentSchema]
-},{
-    timestamps: true
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Currency,
+    required: true,
+    min: 0
+  },
+  featured: {
+    type: Boolean,
+    default: false
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  comments: [commentSchema]
+}, {
+  timestamps: true
 });
 
 var Dishes = mongoose.model('Dish', dishSchema);
